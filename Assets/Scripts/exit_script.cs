e61755e12ffc770bdf06607b2bfc79b56319b3e8
Stephen-PC\Stephen <garrymcgooble@gmail.com>
2019-02-01 22:21:29 -0500

    added collectible items that change pitch when picked up

---
+++ Assets/Scripts/exit_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit_script : MonoBehaviour
+{
+    private AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        skull_pickup.OnPickup += Exit;
+    }
+
+    void Exit()
+    {
+        audioSrc.Play();
+    }
+}
---
+++ Assets/Scripts/skull_pickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skull_pickup : MonoBehaviour
+{
+    private static int skullCount;
+    private static int skullInitial;
+    private AudioSource audioSrc;
+
+    public delegate void PickupAction();
+    public static event PickupAction OnPickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        skullCount++;
+        skullInitial++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            audioSrc.pitch = (1 - ((float)skullCount / skullInitial)) * 2 + 1;
+            audioSrc.Play();
+            skullCount--;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+            if (OnPickup != null && skullCount == 0)
+                OnPickup();
+
+            Destroy(gameObject, audioSrc.clip.length);
+        }
+    }
+}
