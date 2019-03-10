13ae32741d60ba1f448b3272976379e44c5f354a
unknown <aparavath3@gatech.edu>
2019-03-09 22:43:00 -0500

    Adds Shotgun and Pistol Ammo boxes, I've placed one of each in the Demo Maze, near the pickups.

---
+++ Assets/Scripts/ShotgunAmmoBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunAmmoBox : MonoBehaviour
+{
+    public float rotationSpeed = 120.0f;
+    public int ammoPerBox = 10;
+     
+    void Update () {
+        transform.Rotate(Vector3.up,  rotationSpeed * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.tag.Equals("Player")) {
+            Debug.Log("Shotgun ammo get");
+            Global.shotgun_ammo += ammoPerBox;
+            Destroy(gameObject);
+        }
+    }
+}
