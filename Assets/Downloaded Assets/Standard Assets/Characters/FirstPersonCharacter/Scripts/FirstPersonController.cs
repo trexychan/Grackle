9b240231be6c73b904f4e5d697c3223e3f4cb503
zgoddard3 <zgoddard3@gatech.edu>
2019-03-31 11:11:17 -0400

    Fixed sprint input

--- Assets/Downloaded Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Downloaded Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -214,7 +214,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
             // keep track of whether or not the character is walking or running
-            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
+            m_IsWalking = CrossPlatformInputManager.GetAxis("Sprint") < 0.5;
 #endif
             // set the desired speed to be walking or running
             speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
